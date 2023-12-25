1efd60eaf3d1f5384fb624963a71f5a95db70f3f
mervattamer <150426867+mervattamer@users.noreply.github.com>
2023-12-25 13:16:05 +0200

    so many changes idek where to start

--- suduko_main.py
+++ suduko_main.py
@@ -1,8 +1,50 @@
 from collections import deque
 import copy
-
+import pygame
+import time
+#import random_fn
+from merv_input_gui import Grid
+background_image = pygame.image.load('game_bg.jpg')
 global solved_csp
 global csp 
+global board
+
+space_for_tree = ""
+
+s_tree = ""
+
+
+
+
+def print_grid(csp, possible_vals, s_tree, display_gui):
+    soln = s_tree
+
+    
+    for cell in csp.get("variables"):
+        if (len(possible_vals.get(cell)) != 0):
+            soln += str(possible_vals.get(cell)[0])
+        else:
+            soln += str(0)
+        if (cell + 1) % 9 == 0:
+            if cell != 0 and cell != 80:
+                soln += "\n"
+                soln += s_tree
+        else:
+            soln += " "
+    #print(soln)
+    if(display_gui):
+        soln_array = [[int(num) for num in row.split()] for row in soln.strip().split("\n")]
+        board.update_board(soln_array)
+    ##win.fill((255,255,255))
+        win.blit(background_image, (0, 0))
+        board.draw()
+        pygame.display.update()
+    # time.sleep(0.2)
+    
+    
+        
+    
+
 
 def is_valid(grid, row, col, val):
     for i in range(9):
@@ -87,6 +129,8 @@ def forwad_check(csp, variable, possible_vals):
 
             
 def backtrack_ac3(possible_vals, csp):
+    global space_for_tree
+    global s_tree 
     #get unnassigned variable usig MRV
     mrv = 0
     solved = True
@@ -112,6 +156,11 @@ def backtrack_ac3(possible_vals, csp):
                 consistent = False
         if consistent:
             possible_vals.get(unassigned_variable).append(v)
+            space_for_tree = space_for_tree + "--"
+            s_tree= s_tree+ "  "
+            print(space_for_tree + "arc consistent added " + str(v) + " to pos" + str(unassigned_variable)+ "\n" )
+            
+            print_grid(csp, possible_vals, s_tree, display_gui=False)
 
             forward_checked, csp_fc = forwad_check(csp, unassigned_variable, possible_vals)
             if forward_checked:
@@ -120,26 +169,26 @@ def backtrack_ac3(possible_vals, csp):
                     return True
             
             possible_vals.get(unassigned_variable).remove(v) #remove value from soln if forward check failed
+            
+            space_for_tree= space_for_tree.rstrip(space_for_tree[:-3])
+            s_tree = s_tree.rstrip(s_tree[:-3])
+            print(space_for_tree+ "not arc consistent removed " + str(v) + " from pos" + str(unassigned_variable) + "\n")
+            print_grid(csp, possible_vals, s_tree, display_gui=False)
     return False
         
 
 
-            
+def array_to_string(array):
+    result = ""
+    for row in array:
+        result += " ".join(str(num) for num in row)
+        result += "\n"
+    return result          
 
 
 
-def main():
-    grid_string = """
-    5 3 0 0 7 0 0 0 0
-    6 0 0 1 9 5 0 0 0
-    0 9 8 0 0 0 0 6 0
-    8 0 0 0 6 0 0 0 3
-    4 0 0 8 0 3 0 0 1
-    7 0 0 0 2 0 0 0 6
-    0 6 0 0 0 0 2 8 0
-    0 0 0 4 1 9 0 0 5
-    0 0 0 0 8 0 0 7 9
-    """
+def main( grid_a):
+    grid_string = array_to_string(grid_a)
 
     #csp representation
     n_cells = 81
@@ -153,6 +202,7 @@ def main():
         "neighbors": neighbors
     }
     
+    valid_input = True
     #parse string to array
     grid_array = [[int(num) for num in row.split()] for row in grid_string.strip().split("\n")]
 
@@ -162,10 +212,13 @@ def main():
         cell_val = grid_array[row][col]
         grid_array[row][col] = 0   #to avoid considering the cell value when validating
 
+
+        #could change this to strng validation only if there is time
         #early detection of invalid boards
         if cell_val != 0:
             if cell_val < 0 or cell_val >9:
                 print("invalid board")
+                valid_input = False
                 break
             if is_valid(grid_array, row, col ,cell_val):
                 csp.get("domain")[i] = [cell_val]
@@ -173,37 +226,84 @@ def main():
             else:
                 print("invalid board")
                 csp["domain"] = {var: [] for var in variables}
+                valid_input = False
                 break
         
         grid_array[row][col] = cell_val  #return board to its initial form after validation
 
-    
-    #for i in range(n_cells):
+
+        
+        #for i in range(n_cells):
         
         domain_i = csp.get("domain").get(i)
 
         if len(domain_i) == 1:  #append the values 
             possible_vals.get(i).append(domain_i[0])
-        #####
             
+        #####
+    #main_gui(grid_array)       
+
+    if valid_input:
+        if is_arc_consistent(csp):
+            backtrack_ac3(possible_vals, csp)
+        #print output
+        print_grid(csp, possible_vals, s_tree , display_gui = True)
+        
+        
+# grid_string = """
+#     6 0 9 0 4 0 0 0 1
+#     7 1 0 5 0 9 6 0 0
+#     0 5 0 0 0 0 0 0 0
+#     2 0 7 0 8 0 0 9 0
+#     0 0 0 0 6 0 0 2 4
+#     0 6 0 9 0 0 0 0 8
+#     0 0 8 0 0 0 3 0 0
+#     0 0 0 4 0 0 0 0 7
+#     0 0 0 0 5 0 0 0 0
+#     """    
+# grid_a = [[int(num) for num in row.split()] for row in grid_string.strip().split("\n")]
+
+
+
+#main(grid_string) 
+def main_gui(b):
+    #pygame.init()
+    pygame.font.init()
+    global win
+    win = pygame.display.set_mode((540, 540))
+    pygame.display.set_caption("Sudoku")
+    #b = random_fn.random_inp() #grid array
+    global board
+    board = Grid(b, 9, 9, 540, 540, win)
+    #win.fill((255,255,255))
+    win.blit(background_image, (0, 0))
+    board.draw()
+    pygame.display.update()
+    
+    
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            start_time = time.time()
+            main(b)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+
+            print(f"Elapsed Time: {elapsed_time} seconds")
+            time.sleep(5)
+            break
+        break
+        
+                
+        
+        
+        #redraw_window(win, board)
+       
+    pygame.quit()
+
 
     
-    if is_arc_consistent(csp):
-        backtrack_ac3(possible_vals, csp)
-    
 
-    #print output
-        
-    soln = ""
-
-    for cell in csp.get("variables"):
-        soln += str(possible_vals.get(cell)[0])
-
-        if (cell + 1) % 9 == 0:
-            if cell != 0 and cell != 80:
-                soln += "\n"
-        else:
-            soln += " "
-        
-    print(soln)
-main()
+# main(grid_a)
